14fc8fb1d92103d8d4598afc60ebd451545f610d
KarolineRRocha <emaildakarolineribeiro@gmail.com>
2024-12-10 15:48:36 +0000

    Criação das blades, contorllers e funções MeuCondominio e Mensagens

--- ProjetoCondominio/routes/web.php
+++ ProjetoCondominio/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApartmentsComplexController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SocialiteController;
 use Illuminate\Support\Facades\Route;
@@ -17,6 +18,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/meuCondominio', [ApartmentsComplexController::class, 'show'])->name('apartmentsComplex');
 });
 
 require __DIR__ . '/auth.php';
